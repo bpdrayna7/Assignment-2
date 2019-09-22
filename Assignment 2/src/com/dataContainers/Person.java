9102610f3b2d1436bc04da0bcb4933930a7c6e76
bpdrayna7 <bdrayna2@huskers.unl.edu>
2019-09-22 17:43:58 -0500

    person class

--- Assignment 2/src/com/dataContainers/Person.java
+++ Assignment 2/src/com/dataContainers/Person.java
@@ -1,5 +1,69 @@
 package com.dataContainers;
 
-public class Person {
+import java.util.ArrayList;
 
+public class Person {
+	private String personCode;
+	private String firstName;
+	private String lastName;
+	private Address address;
+	private ArrayList<String> email;
+	
+	public Person(String personCode, String firstName, String lastName, Address address, ArrayList<String> email) {
+		super();
+		this.personCode = personCode;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.email = email;
+	}
+
+	public Person(String personCode, String firstName, String lastName, Address address) {
+		super();
+		this.personCode = personCode;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+	}
+
+	public String getPersonCode() {
+		return personCode;
+	}
+
+	public void setPersonCode(String personCode) {
+		this.personCode = personCode;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public ArrayList<String> getEmail() {
+		return email;
+	}
+
+	public void setEmail(ArrayList<String> email) {
+		this.email = email;
+	}
+	
 }
