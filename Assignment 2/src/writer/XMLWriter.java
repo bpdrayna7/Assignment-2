17f2f18c40f076b5e44953e8d863f7f983b6df69
ryanhruby <ryanhruby549@gmail.com>
2019-09-23 00:05:50 -0500

    data folder, XMLWriter

---
+++ Assignment 2/src/writer/XMLWriter.java
@@ -0,0 +1,34 @@
+package writer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import com.dataContainers.Person;
+import com.thoughtworks.xstream.XStream;
+
+public class XMLWriter {
+
+	public void xmlConverter(ArrayList<Person> people) {
+		
+		XStream xstream = new XStream();
+		File xmlFile = new File("data/Persons.xml");
+		PrintWriter xmlPrinter = null;
+		
+		try {
+			xmlPrinter = new PrintWriter(xmlFile);
+		}
+		catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		xstream.alias("person", Person.class);
+		
+		xmlPrinter.write("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n");
+		for(Person p : people) {
+			xmlPrinter.write(xstream.toXML(p));
+		}
+		xmlPrinter.close();
+		
+	}
+}
