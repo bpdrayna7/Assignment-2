f9a96dd96da71923fe8f8807531401a8fb4d69ff
ryanhruby <ryanhruby549@gmail.com>
2019-10-09 16:21:03 -0500

    Started on detailed reports

--- Assignment 2/src/entities/Product.java
+++ Assignment 2/src/entities/Product.java
@@ -8,7 +8,8 @@ public interface Product {
 
 	public abstract double getTax();
 	public abstract double computeSubtotal(DateTime invoiceDate);
-	public double computeGrandtotal(Customer customer, ArrayList<Product> products, double subtotal);
+	public abstract double computeGrandtotal(Customer customer, ArrayList<Product> products, double subtotal);
 	public abstract String getProductCode();
+	public abstract String getProductType();
 	
 }
--- Assignment 2/src/writer/ConsoleWriter.java
+++ Assignment 2/src/writer/ConsoleWriter.java
@@ -2,7 +2,10 @@ package writer;
 
 import java.util.ArrayList;
 
+import entities.Amenity;
 import entities.Invoice;
+import entities.LeaseAgreement;
+import entities.ParkingPass;
 import entities.Product;
 import entities.SaleAgreement;
 
@@ -12,6 +15,7 @@ public class ConsoleWriter {
 		summary.append(String.format("%-8s %-40s %-20s %-13s %-9s %-11s %-11s %-14s\n", 
 				"Invoice", "Customer", "Realtor", "Subtotal", "Fees", "Taxes", "Discount", "Total"));
 		StringBuilder detailedReports = new StringBuilder();
+		detailedReports.append("Individual Invoice Detail Reports\n================================");
 		for(Invoice i:invoices) {
 			//Writes summary
 			double subtotalTotal = 0;
@@ -29,8 +33,62 @@ public class ConsoleWriter {
 					taxTotal*i.getCustomer().getTax(), i.getCustomer().getDiscount(subtotalTotal)*-1, grandtotal));
 			//Writes detailed reports
 			
+			
+			
+			
+			
+			detailedReports.append(String.format("\nInvoice %s\n============================\n", i.getInvoiceCode()));
+			detailedReports.append(String.format("Realtor: %s\nCustomer Info:\n %s\n", i.getRealtor().toString(), i.getCustomer().toDetailedString()));
+			detailedReports.append("-------------------------------------------\n");
+			detailedReports.append(String.format("%-10s %-70s %10s %10s %10s\n", "Code", "Item", "Subtotal", "Tax", "Total"));
+			
+			for(Product p : i.getProducts()) {
+				String description = "";
+				String moreInfo = "";
+				
+				switch(p.getProductType()) {
+				case "A":
+					Amenity amenity = (Amenity)p;
+					description = amenity.getName();
+					break;
+				case "P":
+					ParkingPass pass = (ParkingPass)p;
+					description = "Parking Pass ";
+					break;
+				case "L":
+					LeaseAgreement lease = (LeaseAgreement)p;
+					description = "Lease Agreement ";
+					moreInfo = lease.getUnits() + " units\n";
+					break;
+				case "S":
+					SaleAgreement sale = (SaleAgreement)p;
+					description = "Sale Agreement ";
+					moreInfo = sale.getUnits() + " units\n";
+					break;
+				}
+				
+			detailedReports.append(String.format("%-10s %-70s\n", p.getProductCode(), description));
+			if(p.getProductType().equals("L") || p.getProductType().equals("S")) {
+				detailedReports.append(String.format("%-10s %s", "", moreInfo));
+			}
+//			switch(p.getProductType()) {
+//			case "L":
+//				LeaseAgreement lease = (LeaseAgreement)p;
+//				detailedReports.append(String.format("%-10s %-70s", "", lease.getUnits()));
+//				break;
+//			case "S":
+//				SaleAgreement sale = (SaleAgreement)p;
+//				detailedReports.append(String.format("%-10s %-70s", "", sale.getUnits()));
+//				break;
+//			}
+			
+			
+//			detailedReports.append(String.format("%-10s &-70s", "", moreInfo));
+			}
+			
 		}
 		System.out.println(summary);
+		System.out.println("\n\n\n");
 		System.out.println(detailedReports);
 	}
 }
